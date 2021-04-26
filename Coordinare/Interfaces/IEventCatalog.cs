64d0ec189812b6d741db72fdfa88932eb113b8be
Mikael Tyge Nielsen <mika8024@edu.easj.dk>
2021-04-26 13:49:12 +0100

    Event / Interface Katalog

---
+++ Coordinare/Interfaces/IEventCatalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Threading.Tasks;
+using Coordinare.Models;
+
+namespace Coordinare.Interfaces
+{
+    public interface IEventCatalog
+    {
+        List<Event> Events { get; set; }
+        Task<List<Event>> GetAllEvents();
+        Task<Event> GetEventFromId(int id);
+        void CreateEvent(Event _event);
+        void DeleteEvent(int id);
+        void UpdateEvent(Event _event, int id);
+        Task<T> GetWaitingList<T>();
+    }
+}
---
+++ Coordinare/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coordinare.Models
+{
+    public class Event
+    {
+        public int Event_ID { get; set; }
+        public string Duration  { get; set; }
+        public int Room_ID { get; set; }
+        public string EventName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Eventinfo { get; set; }
+        public int SS_amount { get; set; }
+
+    }
+}
