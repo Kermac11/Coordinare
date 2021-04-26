ea8b489be52a8dadcf4725f333e76549d384991c
Yasin Erkul <yasinerkull@outlook.dk>
2021-04-26 12:56:14 +0200

    Room / IRoomCatalog

---
+++ Coordinare/Interfaces/IRoomCatalog.cs
@@ -0,0 +1,23 @@
+﻿using Coordinare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coordinare.Interfaces
+{
+    public interface IRoomCatalog
+    {
+        List<Room> Rooms { get; set; }
+
+        Task<List<Room>> GetAllRoomsAsync();
+
+        Task<Room> GetRoomsFromIdAsync(string id);
+
+        void CreateRoomAsync(Room room);
+
+        void DeleteRoomAsync(string id);
+
+        void UpdateRoomAsync(Room room, string id);
+    }
+}
