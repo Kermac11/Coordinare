2c70e22ef6dfed5c1420f1add3f06547cdc07486
Yasin Erkul <yasinerkull@outlook.dk>
2021-05-02 14:00:23 +0200

    ROOM S

--- Coordinare/Models/Room.cs
+++ Coordinare/Models/Room.cs
@@ -7,15 +7,21 @@ namespace Coordinare.Models
 {
     public class Room
     {
+        public Room()
+        {
+
+        }
+        public Room(string roomId, int capacity)
+        {
+            Room_ID = roomId;
+            Capacity = capacity;
+        }
+
         public string Room_ID { get; set; }
 
         public int Capacity { get; set; }
 
-        public Room(string roomId, int capacity)
-        {
-            Room_ID = roomId;
-            Capacity = capacity;    
-        }
+        
         
     }
 }
--- Coordinare/Pages/Rooms/Create.cshtml.cs
+++ Coordinare/Pages/Rooms/Create.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Coordinare.Interfaces;
+using Coordinare.Models;
+using Coordinare.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,29 @@ namespace Coordinare.Pages.Rooms
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly IRoomCatalog roomCatalog;
+        [BindProperty] public Room Room { get; set; }
+        List<Room> Rooms { get; set; }
+
+        public CreateModel(IRoomCatalog RoomCatalog) => this.roomCatalog = RoomCatalog;
+
+        public async Task OnGetAsync()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            try
+            {
+                await roomCatalog.CreateRoomAsync(Room);
+                Rooms = await roomCatalog.GetAllRoomsAsync();
+            }
+            catch (Exception)
+            {
+                return Page();
+            }
+            // Skal vende tilbage til start siden når man har oprettet en bruger
+            return RedirectToPage();
         }
     }
 }
