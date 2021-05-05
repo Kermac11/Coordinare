56ad372dd752a129cf8e227a5dfb2c1e4471a47a
katr4 <katr4722@edu.easj.dk>
2021-05-05 12:34:13 +0200

    autogenreret password i create

--- Coordinare/Pages/Users/Create.cshtml.cs
+++ Coordinare/Pages/Users/Create.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Coordinare.Exceptions;
 using Coordinare.Interfaces;
@@ -16,6 +17,8 @@ namespace Coordinare.Pages.Users
         [BindProperty] public User User { get; set; }
         public string InfoText { get; set; }
         public List<User> Users { get; private set; }
+        public string RndPass { get; private set; }
+        
 
         public CreateModel(IUserCatalog userCatalog)
         {
@@ -25,7 +28,19 @@ namespace Coordinare.Pages.Users
         public async Task OnGetAsync()
         {
             InfoText = "Enter new user";
+            RndPass = CreatePassword(5);
             Users = await userCatalog.GetAllUsersAsync();
+        }
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder bld = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                bld.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return bld.ToString();
         }
 
         public async Task<IActionResult> OnPostAsync(User user)
