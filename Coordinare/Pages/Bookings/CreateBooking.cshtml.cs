b1550a620f5cdca18e43fec77a82d99b1edded9c
stofferw123 <christofferwith121001@gmail.com>
2021-05-06 10:36:58 +0200

    daily pull

--- Coordinare/Pages/Bookings/CreateBooking.cshtml.cs
+++ Coordinare/Pages/Bookings/CreateBooking.cshtml.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Coordinare.Interfaces;
+using Coordinare.Models;
+using Coordinare.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,21 @@ namespace Coordinare.Pages.Bookings
 {
     public class CreateBookingModel : PageModel
     {
-        public void OnGet()
+        private IBookingCatalog _service;
+        private IEventCatalog _eventService;
+        private IUserCatalog _userService;
+        public Event Event { get; set; }
+        public User User { get; set; }
+        public CreateBookingModel(IBookingCatalog service, IEventCatalog eventService, IUserCatalog userService)
         {
+            _service = service;
+            _eventService = eventService;
+            _userService = userService;
+        }
+        public void OnGet(int id, int Uid)
+        {
+            Event = _eventService.GetEventFromId(id).Result;
+            User = _userService.GetUserFromIdAsync(Uid).Result;
         }
     }
 }
