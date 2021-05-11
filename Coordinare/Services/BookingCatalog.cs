ef377c961df073bc73d5c6b38561285138dba65c
stofferw123 <christofferwith121001@gmail.com>
2021-05-11 10:44:30 +0200

    booking from user fix

--- Coordinare/Services/BookingCatalog.cs
+++ Coordinare/Services/BookingCatalog.cs
@@ -127,7 +127,7 @@ namespace Coordinare.Services
                         command.Parameters.AddWithValue("@userID", userID);
                         await command.Connection.OpenAsync();
                         SqlDataReader reader = await command.ExecuteReaderAsync();
-                        if (reader.Read())
+                        while (reader.Read())
                         {
                             int bookingid = reader.GetInt32(0);
                             int eventid = reader.GetInt32(1);
