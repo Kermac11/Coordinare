36c7628522b069f653042918b549763a0bd49f03
Mikael Tyge Nielsen <mika8024@edu.easj.dk>
2021-05-20 13:03:26 +0100

    sql changes

---
+++ Coordinare/SQL/Tag Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tags] (
+    [Tag_ID]   INT          IDENTITY (1, 1) NOT NULL,
+    [Event_ID] INT          NOT NULL,
+    [TagName]  VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Tag_ID] PRIMARY KEY CLUSTERED ([Tag_ID] ASC)
+);
+
