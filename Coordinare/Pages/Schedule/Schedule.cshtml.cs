50e9e2f7ad3060e5d34d7498e47b87e84f789ae1
katr4 <katr4722@edu.easj.dk>
2021-05-26 10:24:15 +0200

    fiks i skema

--- Coordinare/Pages/Schedule/Schedule.cshtml.cs
+++ Coordinare/Pages/Schedule/Schedule.cshtml.cs
@@ -34,9 +34,12 @@ namespace Coordinare.Pages.Schedule
             User = await userCatalog.GetUserFromIdAsync((int)id);
             Bookings = await bookingCatalog.GetBookingsFromUser((int)id);
             BookedEvents = await bookingCatalog.GetBookedEvents((int)id);
-            BookedEvents.Sort((e1,e2) => e1.DateTime.CompareTo(e2.DateTime));
-            NumOfDates = BookedEvents.Select(e => e.DateTime).Distinct().ToList();
-
+            if (BookedEvents.Count != 0)
+            {
+                BookedEvents.Sort((e1, e2) => e1.DateTime.CompareTo(e2.DateTime));
+                NumOfDates = BookedEvents.Select(e => e.DateTime).Distinct().ToList();
+            }
+            
             if (User == null)
                 return NotFound();
 
